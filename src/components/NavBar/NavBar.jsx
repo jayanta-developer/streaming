7b449665c3b3b7d051c5f5440d26589379b96236
regurisahithi <jayanta.dey@musquaretech.com>
2023-02-21 00:35:27 +0530

    Revert "Revert "working on redux singup""
    This reverts commit 6f33e836e3017a53090549e593534d0e09ab9c52.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
-// import GoogleAuth from "../GoogleAuth"
-import GoogleSignUp from "../GoogleSignUp"
+import GoogleSignUp from "../GoogleSignUp";
+// import jwt_decode from "jwt-decode";
 import "./Nav.css";
 
 export default function NavBar() {
+  const [user, setUser] = useState("");
+  // const token = localStorage.getItem('token');
+  // if (token) {
+  //   setUser(jwt_decode(token))
+  // }
   return (
     <Box className="NavStyle">
+      {/* <h4>{user ? user.name : ""}</h4> */}
       <Box className="NavItem"><Link to="/">Streams</Link></Box>
       <Box className="NavItem"><Link to="/streams/new">New Streams</Link></Box>
       <Box className="NavItem"><GoogleSignUp /></Box>
