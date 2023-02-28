137ad2288f61549e643afffb5d5bcde7fad2f9a8
regurisahithi <jayanta.dey@musquaretech.com>
2023-02-28 22:43:45 +0530

    fix all pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import App from "./components/App";
-import reducers from "./reducers";
-// import thunk from "redux-thunk";
-
-const store = createStore(reducers);
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
