a7038536e7a5671f82c40df8e15089192a2ad0ef
regurisahithi <jayanta.dey@musquaretech.com>
2023-03-01 09:40:47 +0530

    try to fix product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { getProduct } from "../../store/productSlice"
+import { useSelector } from 'react-redux';
 
 export default function Products() {
+  const dispatch = useDispatch()
+  dispatch(getProduct())
+  const products = useSelector(state => state.products);
+  console.log(products)
+
   return (
     <div>Products</div>
   )
