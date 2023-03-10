07dc069e776ae0a159a952a97aa79d502b5f9897
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-03-10 20:15:56 +0530

    getting err in redux persist

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persister = persistStore(store);
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persister={persister}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
 import productReducer from "./productSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const reducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    products: productReducer,
-  },
+  reducer: persistedReducer,
+  // reducer: {
+  //   products: productReducer,
+  // },
 });
 export default store;
